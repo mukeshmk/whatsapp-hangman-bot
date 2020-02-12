ee1f050ca7d1148cb16071e27a3f620a31aee510
Mukesh A <amukesh.mk@gmail.com>
2020-02-12 00:43:56 +0000

    modified game initial states and cmds

--- bot.py
+++ bot.py
@@ -6,57 +6,62 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 # Initialize game state variables
-count = 0
+count = -1
+game_start = False
 attempts_remaining = 6
 word = ""
-
-idxs = [letter not in ascii_lowercase for letter in word]
-remaining_letters = set(ascii_lowercase)
+idxs = []
+remaining_letters = 0
 wrong_letters = []
 word_solved = False
 
-def init():
-    global count, attempts_remaining, word
+def init_word():
     global idxs, remaining_letters, wrong_letters, word_solved
-
-    count = 0
-    attempts_remaining = 6
-    word = ""
-
     idxs = [letter not in ascii_lowercase for letter in word]
     remaining_letters = set(ascii_lowercase)
     wrong_letters = []
     word_solved = False
 
+def init():
+    global count, attempts_remaining, word, game_start
+    count = -1
+    game_start = False
+    attempts_remaining = 6
+    word = ""
+    init_word()
+
 @app.route('/bot', methods=['POST'])
 def bot():
     # Initalizing global variables in function
-    global count, attempts_remaining, word
+    global count, attempts_remaining, word, game_start
     global idxs, remaining_letters, wrong_letters, word_solved
 
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
 
-    if 'start game' in incoming_msg:
+    if 'start game' in incoming_msg and game_start == False:
         init()
+        game_start = True
 
         msgbody = 'Starting a game of Hangman...\n'
-        msgbody += 'Enter a Word: (word: *your word*)'
-
-        msg.body(msgbody)
-    elif 'quit' in incoming_msg:
+        msgbody += 'Enter a Word: (_word: yourword_)'
+    elif 'quit' in incoming_msg and game_start == True:
         init()
 
         msgbody = 'quiting game!'
-        
-        msg.body(msgbody)
-    elif 'word: ' in incoming_msg:
+    elif 'word: ' in incoming_msg and word == "" and game_start == True:
         word = incoming_msg.split(":")[1]
-        msgbody = word
+        init_word()
+        count = 0
 
-        msg.body(msgbody)
-        
+        msgbody = word
+    elif len(incoming_msg) == 1 and count >= 0:
+        msgbody = incoming_msg
+    else:
+        msgbody = "invalid cmd!"
+
+    msg.body(msgbody)
     return str(resp)
 
 # Run Server
